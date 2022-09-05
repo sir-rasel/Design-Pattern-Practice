6fc64e2269a040a243a658be7d5f25bb61684fd2
Saiful Islam Rasel <raselcse97@gmail.com>
2022-09-05 23:35:01 +0600

    Memento design pattern project initialize

---
+++ MementoDesignPattern/C#/MementoDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MementoDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
