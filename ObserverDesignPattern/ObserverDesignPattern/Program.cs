e9c1818ee71a070ccb383ea03934361e3316e46b
Saiful Islam Rasel <raselcse97@gmail.com>
2022-09-10 21:47:28 +0600

    initiate the observer design pattern project

---
+++ ObserverDesignPattern/ObserverDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
