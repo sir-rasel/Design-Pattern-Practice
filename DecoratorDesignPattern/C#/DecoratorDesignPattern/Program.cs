ee84190d22fc326403e7c4c1826cb7536064d73c
Saiful Islam Rasel <raselcse97@gmail.com>
2022-09-22 00:11:25 +0600

    add the when to use snipets

---
+++ DecoratorDesignPattern/C#/DecoratorDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecoratorDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
